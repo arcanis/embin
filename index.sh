945585ca845d9a8548c1e141fc5d54fbfc0c64cd
Maël Nison <nison.mael@gmail.com>
2020-07-11 01:18:34 +0200

    Uses defaults

--- index.sh
+++ index.sh
@@ -8,8 +8,10 @@ BUILD_DIR="$(mktemp -d)"
 
 EMSDK_MANIFEST="$(curl https://raw.githubusercontent.com/emscripten-core/emsdk/master/emscripten-releases-tags.txt)"
 
-EMSCRIPTEN_TAG="$(jq -r .latest <<< "$EMSDK_MANIFEST")"
-RELEASE_TAG="$(jq -r ".releases[\"$EMSCRIPTEN_TAG\"]" <<< "$EMSDK_MANIFEST")"
+EMSCRIPTEN_TAG="${EMSCRIPTEN_TAG:-$(jq -r .latest <<< "$EMSDK_MANIFEST")}"
+RELEASE_TAG="${RELEASE_TAG:-$EMSCRIPTEN_TAG}"
+
+PREBUILD_TAG="$(jq -r ".releases[\"$EMSCRIPTEN_TAG\"]" <<< "$EMSDK_MANIFEST")"
 
 PLATFORM=linux
 
@@ -19,7 +21,7 @@ cd "$BUILD_DIR"
 yarn init -2
 
 # Install the release
-wget -q https://storage.googleapis.com/webassembly/emscripten-releases-builds/"$PLATFORM"/"$RELEASE_TAG"/wasm-binaries.tbz2
+wget -q https://storage.googleapis.com/webassembly/emscripten-releases-builds/"$PLATFORM"/"$PREBUILD_TAG"/wasm-binaries.tbz2
 tar xf wasm-binaries.tbz2
 
 # Remove various useless items
@@ -30,7 +32,7 @@ rm -f wasm-binaries.tbz2
 rsync -azh "$THIS_DIR"/static/ "$BUILD_DIR"
 
 # Inject the variables
-jq ". + {\"name\": \"embin-$PLATFORM\", \"version\": \"$EMSCRIPTEN_TAG\"}" package.json | sponge package.json
+jq ". + {\"name\": \"embin-$PLATFORM\", \"version\": \"$RELEASE_TAG\"}" package.json | sponge package.json
 
 # Publish time!
 yarn npm publish --tolerate-republish
